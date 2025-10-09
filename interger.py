cc7f7e8336f532c22cb6dbbeeb911226aceb4a1f
meder <mederemilev04@gmail.com>
2025-10-09 22:17:59 +0000

    first commit

---
+++ interger.py
@@ -0,0 +1,25 @@
+num_int1 = 10 
+num_int2 = 5 
+
+print(num_int1 + num_int2)
+print(num_int1 - num_int2)
+print(num_int1 * num_int2)
+print(num_int1 / num_int2)
+
+
+num_float1 = 10.5
+num_float2 = 13.6
+
+print(num_float1 + num_float2)
+print(num_float1 - num_float2)
+print(num_float1 * num_float2)
+print(num_float1 / num_float2)
+
+print(num_int1 // num_int2)
+
+
+print("  ")
+print(0x11a)
+print(0o10)
+print(0b11)
+
