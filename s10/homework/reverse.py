787279b16b69f8a3726d0c72c341cdf79ef555fd
meder <mederemilev04@gmail.com>
2025-10-14 23:00:00 +0000

    start of class

--- s10/homework/reverse.py
+++ s10/homework/reverse.py
@@ -1,3 +1,10 @@
+# Task 2: Reverse a Word using for loop (please don’t reverse a string using word[::-1])
+# Input:
+# Enter a word: Python
+# Output:
+# Reversed Word: nohtyP
+
+
 word = input("enter a word: ")
 reversed_word = ""
 
